951813286ac3ca9550bf1a2bca8fa5e60ffe9e0c
thomasguyader <thomas.guyader@gouv.nc>
2024-01-30 23:33:01 +0000

    chore: gestion déploiement

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM gcr.io/dataflow-templates-base/python311-template-launcher-base
+
+ENV FLEX_TEMPLATE_PYTHON_REQUIREMENTS_FILE="/template/requirements.txt"
+ENV FLEX_TEMPLATE_PYTHON_PY_FILE="/template/postgresql_to_bigquery.py"
+
+COPY requirements.txt /template/
+
+# We could get rid of installing libffi-dev and git, or we could leave them.
+RUN apt-get update \
+    && apt-get install -y openjdk-11-jdk libffi-dev git \
+    && rm -rf /var/lib/apt/lists/* \
+    # Upgrade pip and install the requirements.
+    && pip install --no-cache-dir --upgrade pip \
+    && pip install --no-cache-dir -r $FLEX_TEMPLATE_PYTHON_REQUIREMENTS_FILE \
+    # Download the requirements to speed up launching the Dataflow job.
+    && pip download --no-cache-dir --dest /tmp/dataflow-requirements-cache -r $FLEX_TEMPLATE_PYTHON_REQUIREMENTS_FILE
+
+ENV JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+
+# Since we already downloaded all the dependencies, there's no need to rebuild everything.
+ENV PIP_NO_DEPS=True
+
+COPY src/postgresql_to_bigquery.py /template/
+
+ENTRYPOINT ["/opt/google/dataflow/python_template_launcher"]
--- src/postgresql_to_bigquery.py
+++ src/postgresql_to_bigquery.py
@@ -1,113 +1,123 @@
-from pyspark.sql import SparkSession
-import argparse
 from typing import Optional
-from logging import Logger
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.io.jdbc import ReadFromJdbc
+
+import logging
 
 
-def get_logger(spark: SparkSession) -> Logger:
-    """
-    Convenience method to get the Spark logger from a SparkSession
+class TableUploader(beam.DoFn):
 
-    Args:
-        spark (SparkSession): The initialized SparkSession object
+    def __init__(self, uri, dataset):
+        self.dataset = dataset
+        self.uri = uri
 
-    Returns:
-        Logger: The Spark logger
-    """
-
-    log_4j_logger = spark.sparkContext._jvm.org.apache.log4j  # pylint: disable=protected-access
-    return log_4j_logger.LogManager.getLogger(__name__)
-
-
-def upload_table(spark: SparkSession, table_name: str, url: str, dataset: str, mode: str):
-    get_logger(spark).info("migration table %s" % table_name['table_name'])
-    df = spark.read.jdbc(url, table_name['table_name'], properties={"driver": "org.postgresql.Driver"})
-
-    df.write \
-        .format("bigquery") \
-        .option("writeMethod", "direct") \
-        .mode(mode) \
-        .save("%s.%s" % (dataset, table_name['table_name']))
-
-
-def query_factory(schema: str, exclude: str = None) -> str:
-    if exclude != "":
-        query = "SELECT table_name FROM information_schema.tables where table_schema = '%s' and table_name not in (%s)" % (schema, exclude)
-    else:
-        query = "SELECT table_name FROM information_schema.tables where table_schema = '%s'" % schema
-    return query
-
-
-def run(spark: SparkSession, app_name: Optional[str], schema: str, url: str, dataset: str, mode: str, exclude: str):
-    query = query_factory(schema, exclude)
-
-    table_names = spark.read \
-                       .format("jdbc") \
-                       .option("url", url) \
-                       .option("driver", "org.postgresql.Driver") \
-                       .option("query", query) \
-                       .load()
-
-    get_logger(spark).info("migration de %s tables" % table_names.count())
-
-    for table_name in table_names.collect():
-        upload_table(spark, table_name, url, dataset, mode)
-
-    get_logger(spark).info("fin migration")
+    def process(self, element):
+        import polars as pl
+        from google.cloud import bigquery
+        import io
+        import logging
+        logging.info("traitement de la table %s" % element)
+        uri = self.uri
+        dataset = self.dataset
+        query = "select * from %s" % element[0]
+        df = pl.read_database_uri(query=query, uri=uri)
+        logging.info("contenu récupéré")
+        logging.info(df.head())
+        client = bigquery.Client()  # .from_service_account_json("credentials.json")
+        with io.BytesIO() as stream:
+            df.write_parquet(stream)
+            stream.seek(0)
+            job = client.load_table_from_file(
+                stream,
+                destination='%s.%s' % (dataset, element[0]),
+                # project='prj-dinum-p-exp-mtn-711a',
+                job_config=bigquery.LoadJobConfig(
+                    source_format=bigquery.SourceFormat.PARQUET,
+                ),
+            )
+        print(job.result())
+        return []
 
 
-if __name__ == '__main__':
+def run(
+        schema: str, url: str, dataset: str, mode: str, exclude: str,
+        beam_options: Optional[PipelineOptions] = None,
+        ) -> None:
+    creds = url.split("?user=")
+    uri = creds[0]
+    username, password = creds[1].split("&password=")
+    query = "SELECT table_name FROM information_schema.tables where table_schema = '%s'" % schema
+    print(query)
+    with beam.Pipeline(options=beam_options) as pipeline:
+        tables = pipeline | 'Create table list' >> ReadFromJdbc(
+                            query=query,
+                            driver_class_name='org.postgresql.Driver',
+                            jdbc_url='jdbc:%s' % uri,
+                            username=username,
+                            password=password,
+                            table_name=""
+                        )                           
+        tables | 'traitement de chaques tables' >> beam.ParDo(TableUploader(url, dataset)) # 
+        print(tables)
+
+
+class MyOptions(PipelineOptions):
+    @classmethod
+    def _add_argparse_args(cls, parser):
+        parser.add_argument(
+            '--jdbc-url',
+            type=str,
+            dest='jdbc_url',
+            required=True,
+            help='URL JDBC vers la bdd source')
+
+        parser.add_argument(
+            '--schema',
+            type=str,
+            dest='schema',
+            required=True,
+            help='schéma à migrer')
+
+        parser.add_argument(
+            '--dataset',
+            type=str,
+            dest='dataset',
+            required=True,
+            help='schéma à migrer')
+
+        parser.add_argument(
+            '--mode',
+            type=str,
+            dest='mode',
+            required=False,
+            default="overwrite",
+            help='schéma à migrer')
+
+        parser.add_argument(
+            '--exclude',
+            type=str,
+            dest='exclude',
+            required=False,
+            default="",
+            help='tables à exclure de la migration')
+
+
+if __name__ == "__main__":
+    import argparse
+
+    logging.getLogger().setLevel(logging.INFO)
+
     parser = argparse.ArgumentParser()
+    
+    beam_options = PipelineOptions()
+    args = beam_options.view_as(MyOptions)
 
-    parser.add_argument(
-        '--jdbc-url',
-        type=str,
-        dest='jdbc_url',
-        required=True,
-        help='URL JDBC vers la bdd source')
-
-    parser.add_argument(
-        '--schema',
-        type=str,
-        dest='schema',
-        required=True,
-        help='schéma à migrer')
-
-    parser.add_argument(
-        '--dataset',
-        type=str,
-        dest='dataset',
-        required=True,
-        help='schéma à migrer')
-
-    parser.add_argument(
-        '--mode',
-        type=str,
-        dest='mode',
-        required=False,
-        default="overwrite",
-        help='schéma à migrer')
-
-    parser.add_argument(
-        '--exclude',
-        type=str,
-        dest='exclude',
-        required=False,
-        default="",
-        help='tables à exclure de la migration')
-
-    known_args, pipeline_args = parser.parse_known_args()
-
-    spark = SparkSession.builder \
-        .appName("PostgreSQL Migration with PySpark") \
-        .getOrCreate()
-        #.config("spark.jars", "gs://bucket-df-lsu/postgresql-42.2.6.jar,gs://bucket-df-lsu/spark-3.4-bigquery-0.35.1.jar") \
-        
-
-    run(app_name="database transfert",
-        spark=spark,
-        schema=known_args.schema,
-        url=known_args.jdbc_url,
-        dataset=known_args.dataset,
-        mode=known_args.mode,
-        exclude=known_args.exclude)
+    run(
+        schema=args.schema,
+        url=args.jdbc_url,
+        dataset=args.dataset,
+        mode=args.mode,
+        exclude=args.exclude,
+        beam_options=beam_options,
+    )
