874fa073ce4f3492471ff893c1990e3592a116c6
thomasguyader <thomas.guyader@gouv.nc>
2024-01-30 05:31:35 +0000

    fix(registry): correction référence repo

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,7 +41,7 @@ resource "google_artifact_registry_repository" "template-repo" {
 
 resource "google_artifact_registry_repository_iam_binding" "binding" {
   project    = google_artifact_registry_repository.template-repo.project
-  location   = google_artifact_registry_repository.mtemplate-repo.location
+  location   = google_artifact_registry_repository.template-repo.location
   repository = google_artifact_registry_repository.template-repo.name
   role       = "roles/artifactregistry.reader"
   members = [
